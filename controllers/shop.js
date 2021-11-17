94345236537e88423a00069fbb61565bebf10e49
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2021-11-16 19:42:07 -0500

    add to cart post request for all view that need this functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -23,9 +23,12 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const productId = req.params.productId;
   Product.findById(productId, (product) => {
-    console.log("product", product);
+    res.render("shop/product-detail.ejs", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
+    });
   });
-  res.redirect("/");
 };
 
 exports.getCart = (req, res, next) => {
@@ -33,6 +36,12 @@ exports.getCart = (req, res, next) => {
     pageTitle: "Your Cart",
     path: "/cart",
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId;
+  console.log("productId", productId);
+  res.redirect("/cart");
 };
 
 exports.getOrders = (req, res, next) => {
