6e29edfd00617a6a318390d7026e13565782e034
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2021-12-02 13:49:20 -0500

    display Cart items

--- models/cart.js
+++ models/cart.js
@@ -61,4 +61,16 @@ module.exports = class Cart {
       });
     });
   }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (!err) {
+        cb(cart);
+      } else {
+        console.log(err);
+        cb(null);
+      }
+    });
+  }
 };
